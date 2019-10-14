bd2719f075bc7ceb4e1ecb3d17656c9d5b9fe9f4
GerA <lpgera@gmail.com>
2019-10-14 22:57:21 +0200

    add sdc2-client package

---
+++ packages/sdc2-client/sdc2-client.js
@@ -0,0 +1,58 @@
+const axios = require('axios')
+const log = require('./log')
+
+module.exports = ({ url, apiKey, location }) => {
+  axios.interceptors.response.use(
+    response => {
+      return response
+    },
+    err => {
+      log.error(
+        `sdc2-client request error - status: ${err.response.status} body:`,
+        err.response.data
+      )
+      return Promise.reject(err)
+    }
+  )
+
+  function storeMeasurement({ type, value }) {
+    return axios.post(
+      `${url}/api/measurement/location/${location}`,
+      { type, value },
+      {
+        headers: {
+          'X-API-Key': apiKey,
+        },
+      }
+    )
+  }
+
+  function storeMeasurements({ measurements }) {
+    return axios.post(
+      `${url}/api/measurement/location/${location}`,
+      measurements,
+      {
+        headers: {
+          'X-API-Key': apiKey,
+        },
+      }
+    )
+  }
+
+  function getLatestMeasurement({ location, type }) {
+    return axios.get(
+      `${url}/api/measurement/latest/location/${location}/type/${type}`,
+      {
+        headers: {
+          'X-API-Key': apiKey,
+        },
+      }
+    )
+  }
+
+  return {
+    storeMeasurement,
+    storeMeasurements,
+    getLatestMeasurement,
+  }
+}
