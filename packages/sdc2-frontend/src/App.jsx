e7d3e96a0a477b013d1702741c254620cf650ad9
GerA <lpgera@gmail.com>
2021-04-03 22:23:02 +0200

    update theme

--- packages/sdc2-frontend/src/App.jsx
+++ packages/sdc2-frontend/src/App.jsx
@@ -1,13 +1,27 @@
 import { SnackbarProvider } from 'notistack'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import { AuthProvider } from './AuthContext'
 import Frame from './Frame'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#1a237e',
+    },
+    secondary: {
+      main: '#028090',
+    },
+  },
+})
 
 export default function App() {
   return (
     <AuthProvider>
-      <SnackbarProvider>
-        <Frame />
-      </SnackbarProvider>
+      <ThemeProvider theme={theme}>
+        <SnackbarProvider>
+          <Frame />
+        </SnackbarProvider>
+      </ThemeProvider>
     </AuthProvider>
   )
 }
