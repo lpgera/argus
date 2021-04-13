3eeeb807d79c1374e810e71319a3881413256e70
GerA <lpgera@gmail.com>
2021-04-13 23:15:38 +0200

    add series state management to MeasurementChart component

--- packages/sdc2-frontend/src/MeasurementChart.js
+++ packages/sdc2-frontend/src/MeasurementChart.js
@@ -1,11 +1,15 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import Paper from '@material-ui/core/Paper'
+import { AxiosContext } from './AxiosContext'
 
 export default function MeasurementChart() {
-  const [start] = useState(new Date('2021-04-04').setHours(0, 0, 0, 0))
+  const [start] = useState(new Date().setHours(0, 0, 0, 0))
   const [end] = useState(new Date().setHours(23, 59, 59, 999))
   const { search } = useLocation()
   const [locationsAndTypes, setLocationsAndTypes] = useState([])
+  const [seriesByTypeAndLocation, setSeriesByTypeAndLocation] = useState({})
+  const { axios } = useContext(AxiosContext)
 
   useEffect(() => {
     const urlSearchParams = new URLSearchParams(search)
@@ -20,11 +24,44 @@ export default function MeasurementChart() {
     )
   }, [search])
 
+  useEffect(() => {
+    const getData = async () => {
+      setSeriesByTypeAndLocation({})
+      await Promise.all(
+        locationsAndTypes.map(async ({ location, type }) => {
+          const startIso = new Date(start).toISOString()
+          const endIso = new Date(end).toISOString()
+          const response = await axios.get(
+            `measurement/location/${location}/type/${type}/from/${startIso}/to/${endIso}/aggregation/average`
+          )
+          setSeriesByTypeAndLocation((series) => ({
+            ...series,
+            [type]: {
+              ...series[type],
+              [location]: response.data,
+            },
+          }))
+        })
+      )
+    }
+    if (start && end && locationsAndTypes.length) {
+      getData()
+    }
+  }, [axios, locationsAndTypes, start, end])
+
   return (
     <>
       <h1>Measurements</h1>
 
-      {JSON.stringify({ locationsAndTypes, start, end }, null, 2)}
+      {Object.entries(seriesByTypeAndLocation).map(
+        ([type, seriesByLocation]) => (
+          <Paper style={{ padding: 16, marginBottom: 16 }} key={type}>
+            <h2 style={{ marginTop: 0 }}>{type}</h2>
+
+            {JSON.stringify(seriesByLocation, null, 2)}
+          </Paper>
+        )
+      )}
     </>
   )
 }
