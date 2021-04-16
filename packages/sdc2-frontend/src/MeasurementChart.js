a6f3c5b11835eb5d19fdaf488d277d2f51a5a44f
GerA <lpgera@gmail.com>
2021-04-16 23:01:39 +0200

    add multiple y axes

--- packages/sdc2-frontend/src/MeasurementChart.js
+++ packages/sdc2-frontend/src/MeasurementChart.js
@@ -9,14 +9,43 @@ export default function MeasurementChart() {
   const [end, setEnd] = useState(new Date().setHours(23, 59, 59, 999))
 
   const { search } = useLocation()
+  const [types, setTypes] = useState([])
+  const [yAxes, setYAxes] = useState({})
   const [locationsAndTypes, setLocationsAndTypes] = useState([])
-  const [seriesByTypeAndLocation, setSeriesByTypeAndLocation] = useState({})
+  const [series, setSeries] = useState([])
   const { axios } = useContext(AxiosContext)
 
   useEffect(() => {
     const urlSearchParams = new URLSearchParams(search)
     const locations = urlSearchParams.getAll('location')
     const types = urlSearchParams.getAll('type')
+    const uniqueTypes = [...new Set(types)]
+
+    setTypes(uniqueTypes)
+
+    setYAxes(
+      Object.fromEntries(
+        uniqueTypes.map((type, i) => {
+          if (i === 0) {
+            return [
+              'yaxis',
+              {
+                title: type,
+                fixedrange: true,
+              },
+            ]
+          }
+          return [
+            `yaxis${i + 1}`,
+            {
+              title: type,
+              fixedrange: true,
+              overlaying: 'y',
+            },
+          ]
+        })
+      )
+    )
 
     setLocationsAndTypes(
       locations.map((location, i) => ({
@@ -28,129 +57,119 @@ export default function MeasurementChart() {
 
   useEffect(() => {
     const getData = async () => {
-      setSeriesByTypeAndLocation({})
-      await Promise.all(
+      const newSeries = await Promise.all(
         locationsAndTypes.map(async ({ location, type }) => {
           const startIso = new Date(start).toISOString()
           const endIso = new Date(end).toISOString()
           const response = await axios.get(
             `measurement/location/${location}/type/${type}/from/${startIso}/to/${endIso}/aggregation/average`
           )
-          setSeriesByTypeAndLocation((series) => ({
-            ...series,
-            [type]: {
-              ...series[type],
-              [location]: {
-                type: 'scatter',
-                mode: 'lines',
-                name: `${location} - ${type}`,
-                x: response.data.map(([x]) => new Date(x)),
-                y: response.data.map(([, y]) => y),
-              },
-            },
-          }))
+          const typeIndex = types.indexOf(type)
+          return {
+            type: 'scatter',
+            mode: 'lines',
+            name: `${location} - ${type}`,
+            x: response.data.map(([x]) => new Date(x)),
+            y: response.data.map(([, y]) => y),
+            yaxis: `y${typeIndex + 1}`,
+          }
         })
       )
+
+      setSeries(newSeries)
     }
     if (start && end && locationsAndTypes.length) {
       getData()
     }
-  }, [axios, locationsAndTypes, start, end])
+  }, [axios, locationsAndTypes, start, end, types])
 
   return (
     <>
       <h1>Measurements</h1>
 
-      {Object.entries(seriesByTypeAndLocation).map(
-        ([type, seriesByLocation]) => (
-          <Paper style={{ padding: 16, marginBottom: 16 }} key={type}>
-            <h2 style={{ marginTop: 0 }}>{type}</h2>
-
-            <Plot
-              style={{ width: '100%', height: 400 }}
-              data={Object.values(seriesByLocation)}
-              layout={{
-                autosize: true,
-                margin: {
-                  l: 40,
-                  r: 20,
-                  t: 0,
-                  b: 30,
-                },
-                xaxis: {
-                  type: 'date',
-                  rangeselector: {
-                    buttons: [
-                      {
-                        count: 6,
-                        label: '6h',
-                        step: 'hour',
-                        stepmode: 'backward',
-                      },
-                      {
-                        count: 12,
-                        label: '12h',
-                        step: 'hour',
-                        stepmode: 'backward',
-                      },
-                      {
-                        count: 1,
-                        label: '1d',
-                        step: 'day',
-                        stepmode: 'backward',
-                      },
-                      {
-                        count: 7,
-                        label: '1w',
-                        step: 'day',
-                        stepmode: 'backward',
-                      },
-                      {
-                        count: 1,
-                        label: '1m',
-                        step: 'month',
-                        stepmode: 'backward',
-                      },
-                      {
-                        count: 3,
-                        label: '3m',
-                        step: 'month',
-                        stepmode: 'backward',
-                      },
-                      {
-                        count: 6,
-                        label: '6m',
-                        step: 'month',
-                        stepmode: 'backward',
-                      },
-                      { step: 'all' },
-                    ],
+      <Paper style={{ padding: 16 }}>
+        <Plot
+          style={{ width: '100%', height: 400 }}
+          data={series}
+          layout={{
+            autosize: true,
+            margin: {
+              l: 40,
+              r: 20,
+              t: 10,
+              b: 30,
+            },
+            hovermode: 'x unified',
+            xaxis: {
+              type: 'date',
+              rangeselector: {
+                buttons: [
+                  {
+                    count: 6,
+                    label: '6h',
+                    step: 'hour',
+                    stepmode: 'backward',
                   },
-                  // rangeslider: {},
-                },
-                yaxis: {
-                  fixedrange: true,
-                },
-                legend: {
-                  orientation: 'h',
-                  xanchor: 'center',
-                  yanchor: 'top',
-                  x: 0.5,
-                  y: -0.1,
-                },
-              }}
-              config={{
-                displayModeBar: false,
-              }}
-              useResizeHandler
-              onRelayout={(e) => {
-                setStart(e['xaxis.range[0]'])
-                setEnd(e['xaxis.range[1]'])
-              }}
-            />
-          </Paper>
-        )
-      )}
+                  {
+                    count: 12,
+                    label: '12h',
+                    step: 'hour',
+                    stepmode: 'backward',
+                  },
+                  {
+                    count: 1,
+                    label: '1d',
+                    step: 'day',
+                    stepmode: 'backward',
+                  },
+                  {
+                    count: 7,
+                    label: '1w',
+                    step: 'day',
+                    stepmode: 'backward',
+                  },
+                  {
+                    count: 1,
+                    label: '1m',
+                    step: 'month',
+                    stepmode: 'backward',
+                  },
+                  {
+                    count: 3,
+                    label: '3m',
+                    step: 'month',
+                    stepmode: 'backward',
+                  },
+                  {
+                    count: 6,
+                    label: '6m',
+                    step: 'month',
+                    stepmode: 'backward',
+                  },
+                  { step: 'all' },
+                ],
+              },
+              // rangeslider: {},
+            },
+            ...yAxes,
+            legend: {
+              orientation: 'h',
+              xanchor: 'center',
+              yanchor: 'top',
+              x: 0.5,
+              y: -0.1,
+            },
+          }}
+          config={{
+            displayModeBar: false,
+          }}
+          useResizeHandler
+          onRelayout={(e) => {
+            setStart(e['xaxis.range[0]'])
+            setEnd(e['xaxis.range[1]'])
+          }}
+        />
+      </Paper>
     </>
   )
 }
