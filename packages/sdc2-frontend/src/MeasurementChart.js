4d222a74f6b00a8a4afedc2f837c785990a03800
GerA <lpgera@gmail.com>
2021-04-18 20:19:53 +0200

    set max range of charts

--- packages/sdc2-frontend/src/MeasurementChart.js
+++ packages/sdc2-frontend/src/MeasurementChart.js
@@ -3,16 +3,20 @@ import { useLocation } from 'react-router-dom'
 import Paper from '@material-ui/core/Paper'
 import Plotly from 'plotly.js/dist/plotly-basic'
 import createPlotlyComponent from 'react-plotly.js/factory'
+import debounce from 'lodash/debounce'
 import { AxiosContext } from './AxiosContext'
 
 const Plot = createPlotlyComponent(Plotly)
 
 export default function MeasurementChart() {
-  const [bounds, setBounds] = useState({
+  const [range, setRange] = useState({
     start: new Date().setHours(0, 0, 0, 0),
     end: new Date().setHours(23, 59, 59, 999),
   })
-
+  const [maxRange, setMaxRange] = useState({
+    start: new Date().setHours(0, 0, 0, 0),
+    end: new Date().setHours(23, 59, 59, 999),
+  })
   const { search } = useLocation()
   const [types, setTypes] = useState([])
   const [yAxes, setYAxes] = useState({})
@@ -51,22 +55,47 @@ export default function MeasurementChart() {
         })
       )
     )
+    const locationAndTypes = locations.map((location, i) => ({
+      location,
+      type: types[i],
+    }))
+    setLocationsAndTypes(locationAndTypes)
 
-    setLocationsAndTypes(
-      locations.map((location, i) => ({
-        location,
-        type: types[i],
-      }))
-    )
-  }, [search])
+    const getData = async () => {
+      const newMaxRange = {
+        start: Number.MAX_VALUE,
+        end: 0,
+      }
+      await Promise.all(
+        locationAndTypes.map(async ({ location, type }) => {
+          const { data } = await axios.get(
+            `measurement/location/${location}/type/${type}`
+          )
+          for (const [x] of data) {
+            const start = new Date(x).setHours(0, 0, 0, 0)
+            const end = new Date(x).setHours(23, 59, 59, 999)
+            if (start < newMaxRange.start) {
+              newMaxRange.start = start
+            }
+            if (end > newMaxRange.end) {
+              newMaxRange.end = end
+            }
+          }
+        })
+      )
+      setMaxRange(newMaxRange)
+    }
+
+    getData().catch(console.error)
+  }, [axios, search])
 
   useEffect(() => {
     const getData = async () => {
       const newSeries = await Promise.all(
         locationsAndTypes.map(async ({ location, type }) => {
-          const startIso = new Date(bounds.start).toISOString()
-          const endIso = new Date(bounds.end).toISOString()
-          const response = await axios.get(
+          const startIso = new Date(range.start).toISOString()
+          const endIso = new Date(range.end).toISOString()
+          const { data } = await axios.get(
             `measurement/location/${location}/type/${type}/from/${startIso}/to/${endIso}/aggregation/average`
           )
           const typeIndex = types.indexOf(type)
@@ -74,8 +103,8 @@ export default function MeasurementChart() {
             type: 'scatter',
             mode: 'lines',
             name: `${location} - ${type}`,
-            x: response.data.map(([x]) => new Date(x)),
-            y: response.data.map(([, y]) => y),
+            x: data.map(([x]) => x),
+            y: data.map(([, y]) => y),
             yaxis: `y${typeIndex + 1}`,
           }
         })
@@ -86,7 +115,7 @@ export default function MeasurementChart() {
     if (locationsAndTypes.length) {
       getData().catch(console.error)
     }
-  }, [axios, locationsAndTypes, bounds, types])
+  }, [axios, locationsAndTypes, range, types])
 
   return (
     <>
@@ -94,7 +123,7 @@ export default function MeasurementChart() {
 
       <Paper style={{ padding: 16 }}>
         <Plot
-          style={{ width: '100%', height: 400 }}
+          style={{ width: '100%', height: 550 }}
           data={series}
           layout={{
             autosize: true,
@@ -102,7 +131,7 @@ export default function MeasurementChart() {
               l: 40,
               r: 20,
               t: 10,
-              b: 30,
+              b: 10,
             },
             hovermode: 'x unified',
             hoverlabel: {
@@ -110,6 +139,12 @@ export default function MeasurementChart() {
             },
             xaxis: {
               type: 'date',
+              autorange: false,
+              range: [new Date(range.start), new Date(range.end)],
+              rangeslider: {
+                autorange: false,
+                range: [new Date(maxRange.start), new Date(maxRange.end)],
+              },
               rangeselector: {
                 buttons: [
                   {
@@ -157,7 +192,6 @@ export default function MeasurementChart() {
                   { step: 'all' },
                 ],
               },
-              // rangeslider: {},
             },
             ...yAxes,
             legend: {
@@ -172,16 +206,19 @@ export default function MeasurementChart() {
             displayModeBar: false,
           }}
           useResizeHandler
-          onRelayout={(e) => {
-            const start = e['xaxis.range[0]']
-            const end = e['xaxis.range[1]']
-            if (start && end) {
-              setBounds({
-                start,
-                end,
-              })
-            }
-          }}
+          onRelayout={debounce((e) => {
+            const start = e['xaxis.range[0]'] ?? e['xaxis.range']?.[0]
+            const end = e['xaxis.range[1]'] ?? e['xaxis.range']?.[1]
+
+            setRange({
+              start: start
+                ? Math.max(new Date(start).getTime(), maxRange.start)
+                : range.start,
+              end: end
+                ? Math.min(new Date(end).getTime(), maxRange.end)
+                : range.end,
+            })
+          }, 200)}
         />
       </Paper>
     </>
