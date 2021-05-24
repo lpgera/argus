95a4cc88584e53d4ade0361271f39f40b5406718
GerA <lpgera@gmail.com>
2021-05-24 22:55:07 +0200

    replace pushbullet with a more up-to-date package

--- packages/sdc2-server/src/pushBullet.js
+++ packages/sdc2-server/src/pushBullet.js
@@ -1,6 +1,6 @@
 const config = require('config')
 const moment = require('moment')
-const PushBullet = require('pushbullet')
+const PushBullet = require('@jef/pushbullet')
 
 const pusher = new PushBullet(config.get('pushBullet.apiKey'))
 
@@ -10,7 +10,8 @@ async function sendWarnings(locations) {
   }
   const message = locations
     .map(
-      l => `${l.location} ${l.type} ${moment(l.latestCreatedAt).toISOString()}`
+      (l) =>
+        `${l.location} ${l.type} ${moment(l.latestCreatedAt).toISOString()}`
     )
     .join('\n')
   await pusher.note('', 'SDC warning', message)
