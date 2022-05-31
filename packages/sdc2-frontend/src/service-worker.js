31821c4761051dab93b3735992a2c42f5120b59f
GerA <lpgera@gmail.com>
2022-05-31 20:03:20 +0200

    fix comment formatting

--- packages/sdc2-frontend/src/service-worker.js
+++ packages/sdc2-frontend/src/service-worker.js
@@ -31,16 +31,16 @@ registerRoute(
     // If this isn't a navigation, skip.
     if (request.mode !== 'navigate') {
       return false
-    } // If this is a URL that starts with /_, skip.
-
+    }
+    // If this is a URL that starts with /_, skip.
     if (url.pathname.startsWith('/_')) {
       return false
-    } // If this looks like a URL for a resource, because it contains // a file extension, skip.
-
+    }
+    // If this looks like a URL for a resource, because it contains // a file extension, skip.
     if (url.pathname.match(fileExtensionRegexp)) {
       return false
-    } // Return true to signal that we want to use the handler.
-
+    }
+    // Return true to signal that we want to use the handler.
     return true
   },
   createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
