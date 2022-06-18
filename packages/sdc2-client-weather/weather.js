9b11d5cdf1bde2bee4f9f06ccdf2e58d5d9f601d
GerA <lpgera@gmail.com>
2022-06-18 14:50:16 +0200

    refactor weather module

--- packages/sdc2-client-weather/weather.js
+++ packages/sdc2-client-weather/weather.js
@@ -10,54 +10,59 @@ const client = Client({
 })
 const log = Logger({ name: 'sdc2-client-weather' })
 
+const getOpenWeatherMapMeasurements = async () => {
+  const openWeatherMapResponse = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?${new URLSearchParams({
+      appid: process.env.WEATHER_OPENWEATHERMAP_API_KEY,
+      lat: process.env.WEATHER_LATITUDE,
+      lon: process.env.WEATHER_LONGITUDE,
+      units: process.env.WEATHER_OPENWEATHERMAP_UNITS,
+    })}`
+  )
+
+  if (!openWeatherMapResponse.ok) {
+    throw new Error(
+      `OpenWeatherMap API error: ${await openWeatherMapResponse.text()}`
+    )
+  }
+
+  const responseData = await openWeatherMapResponse.json()
+
+  return [
+    { type: 'humidity', value: responseData.main.humidity },
+    { type: 'pressure', value: responseData.main.pressure },
+    { type: 'temperature', value: responseData.main.temp },
+    { type: 'wind', value: responseData.wind.speed },
+  ]
+}
+
+const getAirVisualMeasurements = async () => {
+  const airVisualResponse = await fetch(
+    `https://api.airvisual.com/v2/nearest_city?${new URLSearchParams({
+      key: process.env.WEATHER_AIRVISUAL_API_KEY,
+      lat: process.env.WEATHER_LATITUDE,
+      lon: process.env.WEATHER_LONGITUDE,
+    })}`
+  )
+  if (!airVisualResponse.ok) {
+    throw new Error(`AirVisual API error: ${await airVisualResponse.text()}`)
+  }
+
+  const responseData = await airVisualResponse.json()
+
+  return [
+    {
+      type: 'aqi',
+      value: responseData.data.current.pollution.aqius,
+    },
+  ]
+}
+
 const onTick = async () => {
   try {
     const measurementResults = await Promise.allSettled([
-      fetch(
-        `https://api.openweathermap.org/data/2.5/weather?${new URLSearchParams({
-          appid: process.env.WEATHER_OPENWEATHERMAP_API_KEY,
-          lat: process.env.WEATHER_LATITUDE,
-          lon: process.env.WEATHER_LONGITUDE,
-          units: process.env.WEATHER_OPENWEATHERMAP_UNITS,
-        })}`
-      ).then(async (openWeatherMapResponse) => {
-        if (!openWeatherMapResponse.ok) {
-          throw new Error(
-            `OpenWeatherMap API error: ${await openWeatherMapResponse.text()}`
-          )
-        }
-
-        const responseData = await openWeatherMapResponse.json()
-
-        return [
-          { type: 'humidity', value: responseData.main.humidity },
-          { type: 'pressure', value: responseData.main.pressure },
-          { type: 'temperature', value: responseData.main.temp },
-          { type: 'wind', value: responseData.wind.speed },
-        ]
-      }),
-      fetch(
-        `https://api.airvisual.com/v2/nearest_city?${new URLSearchParams({
-          key: process.env.WEATHER_AIRVISUAL_API_KEY,
-          lat: process.env.WEATHER_LATITUDE,
-          lon: process.env.WEATHER_LONGITUDE,
-        })}`
-      ).then(async (airVisualResponse) => {
-        if (!airVisualResponse.ok) {
-          throw new Error(
-            `AirVisual API error: ${await airVisualResponse.text()}`
-          )
-        }
-
-        const responseData = await airVisualResponse.json()
-
-        return [
-          {
-            type: 'aqi',
-            value: responseData.data.current.pollution.aqius,
-          },
-        ]
-      }),
+      getOpenWeatherMapMeasurements(),
+      getAirVisualMeasurements(),
     ])
 
     measurementResults
