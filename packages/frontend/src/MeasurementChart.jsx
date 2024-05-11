69897f21420491ea39a87d5a98fac4be8482aac1
GerA <lpgera@gmail.com>
2024-05-11 17:07:20 +0200

    switch to highstock chart

--- packages/frontend/src/MeasurementChart.jsx
+++ packages/frontend/src/MeasurementChart.jsx
@@ -1,7 +1,6 @@
-import { useEffect, useReducer, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { useTheme } from '@mui/material/styles'
-import styled from '@emotion/styled'
 import Paper from '@mui/material/Paper'
 import Grid from '@mui/material/Grid'
 import FormControl from '@mui/material/FormControl'
@@ -9,107 +8,94 @@ import InputLabel from '@mui/material/InputLabel'
 import Select from '@mui/material/Select'
 import MenuItem from '@mui/material/MenuItem'
 import Button from '@mui/material/Button'
-import Plotly from 'plotly.js-basic-dist'
-import createPlotlyComponent from 'react-plotly.js/factory'
-import debounce from './debounce'
-import useSize from './hooks/useSize'
+import HighchartsReact from 'highcharts-react-official'
+import HighStock from 'highcharts/highstock'
+import 'highcharts/css/highcharts.css'
+import './highcharts.overrides.css'
 import useApiClient from './hooks/useApiClient'
-import './plotly-overrides.css'
-
-const Plot = createPlotlyComponent(Plotly)
-
-const StyledPlot = styled(Plot)(({ theme }) => ({
-  width: '100%',
-  height: 360,
-  marginBottom: theme.spacing(1),
-}))
-
-const colors = [
-  '#2196f3',
-  '#e91e63',
-  '#4caf50',
-  '#607d8b',
-  '#ff9800',
-  '#673ab7',
-  '#cddc39',
-  '#009688',
-  '#3f51b5',
-  '#9c27b0',
-  '#795548',
-]
+import useDarkMode from './hooks/useDarkMode'
 
 const rangeSelectorButtons = [
   {
     count: 6,
-    label: '6h',
-    step: 'hour',
-    stepmode: 'backward',
+    text: '6h',
+    type: 'hour',
   },
   {
     count: 12,
-    label: '12h',
-    step: 'hour',
-    stepmode: 'backward',
+    text: '12h',
+    type: 'hour',
   },
   {
-    count: 1,
-    label: '1d',
-    step: 'day',
-    stepmode: 'backward',
+    text: '1d',
+    type: 'day',
   },
   {
     count: 7,
-    label: '1w',
-    step: 'day',
-    stepmode: 'backward',
+    text: '1w',
+    type: 'day',
   },
   {
-    count: 1,
-    label: '1m',
-    step: 'month',
-    stepmode: 'backward',
+    text: '1m',
+    type: 'month',
   },
   {
     count: 3,
-    label: '3m',
-    step: 'month',
-    stepmode: 'backward',
+    text: '3m',
+    type: 'month',
   },
   {
     count: 6,
-    label: '6m',
-    step: 'month',
-    stepmode: 'backward',
+    text: '6m',
+    type: 'month',
   },
   {
-    count: 1,
-    label: '1y',
-    step: 'year',
-    stepmode: 'backward',
+    text: '1y',
+    type: 'year',
+  },
+  {
+    type: 'all',
+    text: 'All',
   },
 ]
 
 export default function MeasurementChart() {
+  const [isDarkMode] = useDarkMode()
+  const dayStart = new Date().setHours(0, 0, 0, 0)
+  const dayEnd = new Date().setHours(23, 59, 59, 999)
   const theme = useTheme()
   const [, apiClient] = useApiClient()
   const [aggregation, setAggregation] = useState('average')
   const { search } = useLocation()
   const [types, setTypes] = useState([])
   const [locationsAndTypes, setLocationsAndTypes] = useState([])
-  const [maxRange, setMaxRange] = useState({
-    start: new Date().setHours(0, 0, 0, 0),
-    end: new Date().setHours(23, 59, 59, 999),
+  const [extremes, setExtremes] = useState({
+    min: dayStart,
+    max: dayEnd,
   })
-  const [yAxes, setYAxes] = useState({})
-  const [start, setStart] = useState(new Date().setHours(0, 0, 0, 0))
-  const [end, setEnd] = useState(new Date().setHours(23, 59, 59, 999))
-  const [series, setSeries] = useState([])
-  const [refetchState, refetchSeries] = useReducer(() => ({}), {})
+  const [dataSeries, setDataSeries] = useState([])
+  const [navigatorSeries, setNavigatorSeries] = useState([])
+  const [refetchState, setRefetchState] = useState({})
+  const refetchSeries = () => setRefetchState({})
 
-  const chartRef = useRef(null)
+  const yAxis = types.map((type) => ({
+    id: type,
+    title: {
+      text: type,
+    },
+    maxPadding: 0,
+    minPadding: 0,
+    showLastLabel: true,
+    opposite: false,
+  }))
 
-  const { width: chartWidth = 800 } = useSize(chartRef) ?? {}
-  const yAxisWidth = 44.0 / chartWidth
+  const isAllSeriesInitialized =
+    locationsAndTypes.length === dataSeries.length &&
+    locationsAndTypes.length === navigatorSeries.length
+
+  const series = isAllSeriesInitialized
+    ? [...dataSeries, ...navigatorSeries]
+    : []
 
   // calculate locations and types from search
   useEffect(() => {
@@ -127,186 +113,141 @@ export default function MeasurementChart() {
     )
   }, [search])
 
-  // calculate yAxes from types and window size
-  useEffect(() => {
-    setYAxes(
-      Object.fromEntries(
-        types.map((type, i) => {
-          const commonProps = {
-            fixedrange: true,
-            zeroline: false,
-            showgrid: false,
-            color: theme.palette.text.secondary,
-            titlefont: {
-              size: 10,
-            },
-          }
-          if (i === 0) {
-            return [
-              'yaxis',
-              {
-                title: type,
-                ...commonProps,
-                showgrid: true,
-              },
-            ]
-          }
-          return [
-            `yaxis${i + 1}`,
-            {
-              title: type,
-              overlaying: 'y',
-              position: yAxisWidth * i,
-              ...commonProps,
-            },
-          ]
-        })
-      )
-    )
-  }, [types, yAxisWidth, theme])
-
-  // fetch max range based on locations and types
-  useEffect(() => {
-    const fetchMaxRange = async () => {
-      if (locationsAndTypes.length) {
-        const newMaxRange = {
-          start: Number.MAX_VALUE,
-          end: 0,
-        }
-        await Promise.all(
-          locationsAndTypes.map(async ({ location, type }) => {
-            const data = await apiClient(
-              `/measurement/location/${location}/type/${type}`
-            )
-            for (const [x] of data) {
-              const start = new Date(x).setHours(0, 0, 0, 0)
-              const end = new Date(x).setHours(23, 59, 59, 999)
-              if (start < newMaxRange.start) {
-                newMaxRange.start = start
-              }
-              if (end > newMaxRange.end) {
-                newMaxRange.end = end
-              }
-            }
-          })
-        )
-        setMaxRange(newMaxRange)
-      }
-    }
-
-    fetchMaxRange().catch(console.error)
-  }, [apiClient, locationsAndTypes])
-
   // fetch data series
   useEffect(() => {
-    const fetchSeries = async () => {
+    const fetchDataSeries = async () => {
       const newSeries = await Promise.all(
         locationsAndTypes.map(async ({ location, type }) => {
-          const startIso = new Date(start).toISOString()
-          const endIso = new Date(end).toISOString()
+          const startISO = new Date(extremes.min).toISOString()
+          const endISO = new Date(extremes.max).toISOString()
           const data = await apiClient(
-            `/measurement/location/${location}/type/${type}/from/${startIso}/to/${endIso}/aggregation/${aggregation}`
+            `/measurement/location/${location}/type/${type}/from/${startISO}/to/${endISO}/aggregation/${aggregation}`
           )
-          const typeIndex = types.indexOf(type)
           return {
-            type: 'scatter',
-            mode: 'lines',
             name: `${location} - ${type}`,
-            x: data.map(([x]) => x),
-            y: data.map(([, y]) => y),
-            yaxis: `y${typeIndex + 1}`,
-            hovertemplate: '%{y:.2f}',
+            data,
+            yAxis: type,
+            showInNavigator: false,
           }
         })
       )
 
-      setSeries(newSeries)
+      setDataSeries(newSeries)
     }
     if (locationsAndTypes.length) {
-      fetchSeries().catch(console.error)
+      fetchDataSeries().catch(console.error)
     }
-  }, [
-    apiClient,
-    locationsAndTypes,
-    start,
-    end,
-    aggregation,
-    types,
-    refetchState,
-  ])
+  }, [apiClient, locationsAndTypes, extremes, aggregation, types, refetchState])
+
+  // fetch navigator series
+  useEffect(() => {
+    const fetchNavigatorSeries = async () => {
+      const newSeries = await Promise.all(
+        locationsAndTypes.map(async ({ location, type }, i) => {
+          const data = await apiClient(
+            `/measurement/location/${location}/type/${type}`
+          )
+          return {
+            name: `${location} - ${type}`,
+            data,
+            yAxis: type,
+            navigatorOptions: {
+              visible: true,
+            },
+            colorIndex: i,
+            showInNavigator: true,
+            showInLegend: false,
+            visible: false,
+          }
+        })
+      )
+
+      setNavigatorSeries(newSeries)
+    }
+    if (locationsAndTypes.length) {
+      fetchNavigatorSeries().catch(console.error)
+    }
+  }, [apiClient, locationsAndTypes, types, refetchState])
 
   return (
     <>
       <h1>Measurements</h1>
 
-      <Paper style={{ padding: theme.spacing(2) }} ref={chartRef}>
-        <StyledPlot
-          data={series}
-          layout={{
-            plot_bgcolor: theme.palette.background.default,
-            paper_bgcolor: theme.palette.background.paper,
-            autosize: true,
-            dragmode: 'pan',
-            font: {
-              size: 10,
-              color: theme.palette.text.secondary,
+      <Paper
+        style={{ padding: theme.spacing(2) }}
+        className={`highcharts-${isDarkMode ? 'dark' : 'light'}`}
+      >
+        <HighchartsReact
+          containerProps={{ style: { marginBottom: theme.spacing(1) } }}
+          highcharts={HighStock}
+          constructorType={'stockChart'}
+          options={{
+            chart: {
+              type: 'line',
+              animation: false,
+              styledMode: true,
+              marginTop: 20,
             },
-            margin: {
-              l: 0,
-              r: 10,
-              t: 10,
-              b: 10,
+            accessibility: {
+              enabled: false,
             },
-            hovermode: 'x unified',
-            hoverlabel: {
-              namelength: -1,
-            },
-            xaxis: {
-              type: 'date',
-              domain: [types.length * yAxisWidth, 1],
-              showgrid: false,
-              autorange: false,
-              range: [new Date(start), new Date(end)],
-              rangeslider: {
-                autorange: false,
-                thickness: 0.05,
-                range: [new Date(maxRange.start), new Date(maxRange.end)],
-              },
-              rangeselector: {
-                x: 0,
-                y: 1.05,
-                bgcolor: theme.palette.background.default,
-                buttons: rangeSelectorButtons,
+            plotOptions: {
+              series: {
+                dataGrouping: {
+                  enabled: false,
+                },
+                animation: false,
               },
             },
-            ...yAxes,
-            colorway: colors,
-            showlegend: true,
+            time: {
+              timezoneOffset: new Date().getTimezoneOffset(),
+            },
+            rangeSelector: {
+              inputEnabled: false,
+              dropdown: 'never',
+              buttonPosition: {
+                align: 'right',
+                y: -10,
+              },
+              buttons: rangeSelectorButtons,
+            },
+            xAxis: [
+              {
+                type: 'datetime',
+                ordinal: false,
+                min: extremes.min,
+                max: extremes.max,
+                events: {
+                  afterSetExtremes: ({ min, max }) =>
+                    setExtremes({
+                      min: new Date(min ?? dayStart).getTime(),
+                      max: new Date(max ?? dayEnd).getTime(),
+                    }),
+                },
+              },
+            ],
+            yAxis,
+            series,
+            tooltip: {
+              split: false,
+              shared: true,
+            },
             legend: {
-              orientation: 'h',
-              xanchor: 'center',
-              x: 0.5,
-              y: -0.3,
+              enabled: true,
+            },
+            navigator: {
+              adaptToUpdatedData: false,
+              xAxis: {
+                overscroll: 86400000,
+              },
+            },
+            scrollbar: {
+              enabled: true,
+              liveRedraw: false,
+              buttonsEnabled: true,
+              height: 16,
             },
           }}
-          config={{
-            displayModeBar: false,
-            showTips: false,
-          }}
-          useResizeHandler
-          onRelayout={debounce((e) => {
-            const newStart = e['xaxis.range[0]'] ?? e['xaxis.range']?.[0]
-            const newEnd = e['xaxis.range[1]'] ?? e['xaxis.range']?.[1]
-
-            setStart(
-              newStart
-                ? Math.max(new Date(newStart).getTime(), maxRange.start)
-                : start
-            )
-            setEnd(
-              newEnd ? Math.min(new Date(newEnd).getTime(), maxRange.end) : end
-            )
-          }, 200)}
         />
         <Grid container>
           <Grid item xs={6}>
@@ -329,9 +270,7 @@ export default function MeasurementChart() {
             <Button
               variant="contained"
               color="secondary"
-              onClick={() => {
-                refetchSeries()
-              }}
+              onClick={() => refetchSeries()}
             >
               Refresh
             </Button>
